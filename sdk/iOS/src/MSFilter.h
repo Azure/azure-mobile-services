6b2f0314dbb008ab3a32a35f530a492f53bf0896
Randall Tombaugh <randallt@microsoft.com>
2012-10-08 11:42:37 -0700

    Project now builds a universal static framework.

--- sdk/iOS/src/MSFilter.h
+++ sdk/iOS/src/MSFilter.h
@@ -17,12 +17,27 @@
 #import <Foundation/Foundation.h>
 #import "MSError.h"
 
+
+#pragma mark * Block Type Definitions
+
+
+// Callback that the filter should invoke once an HTTP response (with or
+// without data) has been received by the filter.
 typedef void (^MSFilterResponseBlock)(NSHTTPURLResponse *response, NSData *data);
 
+// Callback that the filter should invoke to allow the next filter to handle
+// the given request.
 typedef void (^MSFilterNextBlock)(NSURLRequest *request,
                                   MSFilterResponseBlock onResponse,
                                   MSErrorBlock onError);
 
+
+#pragma  mark * MSFilter Public Protocol
+
+
+// The |MSFilter| protocol allows developers to implement a class that can
+// inspect and/or replace HTTP request and HTTP response messages being sent
+// and received by an |MSClient| instance.
 @protocol MSFilter <NSObject>
 
 -(void) handleRequest:(NSURLRequest *)request
