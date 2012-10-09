e2eef4ca571829182d2d5c5da55363eefb8dd9ed
Roger Cheng <rogerch@microsoft.com>
2012-10-09 16:57:35 -0700

    First draft: Client SDK for Windows Phone 8
    Windows Azure Mobile Services
    Client SDK for Windows Phone 8
    
    First draft

---
+++ sdk/WindowsPhone8/src/Microsoft.Azure.Zumo.WindowsPhone8.WinMD/Core/Resources.cs
@@ -0,0 +1,72 @@
+﻿// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+using System;
+
+namespace Microsoft.WindowsAzure.MobileServices
+{
+    /// <summary>
+    /// Provides access to localizable resources.
+    /// </summary>
+    internal static partial class Resources
+    {
+        // TODO: Pull these from resources once we figure out how to get
+        // .resw's properly included in the resources.pri for a .winmd
+
+        /// <summary>
+        /// Gets a format string for throwing ArgumentExceptions when an
+        /// argument is empty.
+        /// </summary>
+        public static string EmptyArgumentExceptionMessage
+        {
+            get { return "{0} cannot be empty."; }
+        }
+
+        /// <summary>
+        /// Gets a format string for throwing ArgumentExceptions when a JSON
+        /// object is passed for serialization without an ID member.
+        /// </summary>
+        public static string IdNotFoundExceptionMessage
+        {
+            get { return "Expected {0} member not found."; }
+        }
+
+        /// <summary>
+        /// Gets a format string for throwing ArgumentExceptions when an object
+        /// is passed for insertion and has its ID already set.
+        /// </summary>
+        public static string CannotInsertWithExistingIdMessage
+        {
+            get { return "Cannot insert if the {0} member is already set."; }
+        }
+
+        /// <summary>
+        /// Gets a format string for throwing InvalidOperationExceptions when
+        /// a web request results in a failure.
+        /// </summary>
+        public static string MobileServiceClient_ThrowInvalidResponse_ErrorMessage
+        {
+            get { return "{2}  ({0} {1} - Details: {3})"; }
+        }
+
+        /// <summary>
+        /// Gets a format string for throwing InvalidOperationExceptions when
+        /// a web request results in a failure.
+        /// </summary>
+        public static string MobileServiceClient_ThrowConnectionFailure_ErrorMessage
+        {
+            get { return "The request could not be completed.  ({0})"; }
+        }
+
+        /// <summary>
+        /// Gets a format string for throwing ArgumentOutOfRangeExceptions when
+        /// asked to serialize a long/ulong requiring more precision than a
+        /// double.
+        /// </summary>
+        public static string JsonExtensions_TrySetValue_CannotRoundtripNumericValue
+        {
+            get { return "The value {0} for member {1} is outside the valid range for numeric columns."; }
+        }
+    }
+}
---
+++ sdk/WindowsPhone8/test/Microsoft.Azure.Zumo.WindowsPhone8.CSharp.Test/MainPage.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Azure.Zumo.WindowsPhone8.CSharp.Test.Resources;
+
+using Microsoft.Azure.Zumo.WindowsPhone8.Test;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Zumo.WindowsPhone8.CSharp.Test
+{
+    public partial class MainPage : PhoneApplicationPage, ITestReporter
+    {
+        private ObservableCollection<GroupDescription> _groups;
+        private GroupDescription _currentGroup = null;
+        private TestDescription _currentTest = null;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Setup the groups data source
+            _groups = new ObservableCollection<GroupDescription>();
+            cvsTests.Source = _groups;
+
+            // Start a test run
+            App.Harness.Reporter = this;
+            Task.Factory.StartNew(() => App.Harness.RunAsync());
+        }
+        public void StartRun(TestHarness harness)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                lblCurrentTestNumber.Text = harness.Progress.ToString();
+                lblTotalTestNumber.Text = harness.Count.ToString();
+                lblFailureNumber.Tag = harness.Failures.ToString() ?? "0";
+                //progress.Value = 1;
+                //pnlFooter.Visibility = Visibility.Visible;
+            });
+        }
+
+        public void EndRun(TestHarness harness)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                //pnlFooter.Visibility = Visibility.Collapsed;
+                if (harness.Failures > 0)
+                {
+                    lblResults.Text = string.Format(CultureInfo.InvariantCulture, "{0}/{1} tests failed!", harness.Failures, harness.Count);
+                    lblResults.Foreground = new SolidColorBrush(Color.FromArgb(0xFF, 0xFF, 0x00, 0x6E));
+                }
+                else
+                {
+                    lblResults.Text = string.Format(CultureInfo.InvariantCulture, "{0} tests passed!", harness.Count);
+                }
+                lblResults.Visibility = Visibility.Visible;
+            });
+        }
+
+        public void Progress(TestHarness harness)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                lblCurrentTestNumber.Text = harness.Progress.ToString();
+                lblFailureNumber.Text = " " + (harness.Failures.ToString() ?? "0");
+                double value = harness.Progress;
+                int count = harness.Count;
+                if (count > 0)
+                {
+                    value = value * 100.0 / (double)count;
+                }
+                //progress.Value = value;
+            });
+        }
+
+        public void StartGroup(TestGroup group)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                _currentGroup = new GroupDescription { Name = group.Name };
+                _groups.Add(_currentGroup);
+            });
+        }
+
+        public void EndGroup(TestGroup group)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                _currentGroup = null;
+            });
+        }
+
+        public void StartTest(TestMethod test)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                _currentTest = new TestDescription { Name = test.Name };
+                _currentGroup.Tests.Add(_currentTest);
+
+                Dispatcher.BeginInvoke(() =>
+                {
+                    //lstTests.ScrollIntoView(_currentTest);
+                    tbOutput.Text = tbOutput.Text + '\n' + test.Name;
+                });
+            });
+        }
+
+        public void EndTest(TestMethod method)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                if (method.Excluded)
+                {
+                    _currentTest.Color = Color.FromArgb(0xFF, 0x66, 0x66, 0x66);
+                }
+                else if (!method.Passed)
+                {
+                    _currentTest.Color = Color.FromArgb(0xFF, 0xFF, 0x00, 0x6E);
+                }
+                else
+                {
+                    _currentTest.Color = Color.FromArgb(0xFF, 0x2A, 0x9E, 0x39);
+                }
+                _currentTest = null;
+            });
+        }
+
+        public void Log(string message)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                _currentTest.Details.Add(message);
+            });
+        }
+
+        public void Error(string errorDetails)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                _currentTest.Details.Add(errorDetails);
+            });
+        }
+
+        public void Status(string status)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                lblStatus.Text = status;
+            });
+        }
+    }
+}
