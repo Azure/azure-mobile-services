b1cb95541d03e88c38cae8bea14e3c96c75ac40e
Yavor Georgiev <yavorg@microsoft.com>
2013-02-27 16:15:15 -0800

    Adding scripts to automatically download dependencies

---
+++ quickstart/android/ZUMOAPPNAME/libs/getLibs.ps1
@@ -0,0 +1,56 @@
+$client = new-object System.Net.WebClient 
+$shell_app = new-object -com shell.application
+
+Write-Host "Downloading Google Gson 2.2.2"
+$client.DownloadFile("http://google-gson.googlecode.com/files/google-gson-2.2.2-release.zip", "google-gson-2.2.2-release.zip") 
+
+Write-Host "Decompressing..."
+$filename = "google-gson-2.2.2-release.zip"
+$zip_file = $shell_app.namespace((Get-Location).Path + "\$filename")
+$destination = $shell_app.namespace((Get-Location).Path)
+$destination.Copyhere($zip_file.items())
+
+Write-Host "Removing zip"
+Remove-Item google-gson-2.2.2-release.zip
+
+Write-Host "Move library to destination"
+Move-Item .\google-gson-2.2.2\gson-2.2.2.jar .\
+
+Write-Host "Remove extra files"
+Remove-Item .\google-gson-2.2.2\ -recurse
+
+Write-Host "Downloading Apache HttpClient 4.2.3"
+$client.DownloadFile("http://apache.xfree.com.ar//httpcomponents/httpclient/binary/httpcomponents-client-4.2.3-bin.zip", "httpcomponents-client-4.2.3-bin.zip") 
+
+Write-Host "Decompressing..."
+$filename = "httpcomponents-client-4.2.3-bin.zip"
+$zip_file = $shell_app.namespace((Get-Location).Path + "\$filename")
+$destination = $shell_app.namespace((Get-Location).Path)
+$destination.Copyhere($zip_file.items())
+
+Write-Host "Removing zip"
+Remove-Item httpcomponents-client-4.2.3-bin.zip
+
+Write-Host "Move library to destination"
+Move-Item .\httpcomponents-client-4.2.3\lib\httpclient-4.2.3.jar .\
+
+Write-Host "Remove extra files"
+Remove-Item .\httpcomponents-client-4.2.3\ -recurse
+
+Write-Host "Downloading Android Support V4 R11"
+$client.DownloadFile("https://dl-ssl.google.com/android/repository/support_r11.zip", "support_r11.zip") 
+
+Write-Host "Decompressing..."
+$filename = "support_r11.zip"
+$zip_file = $shell_app.namespace((Get-Location).Path + "\$filename")
+$destination = $shell_app.namespace((Get-Location).Path)
+$destination.Copyhere($zip_file.items())
+
+Write-Host "Removing zip"
+Remove-Item support_r11.zip
+
+Write-Host "Move library to destination"
+Move-Item .\support\v4\android-support-v4.jar .\
+
+Write-Host "Remove extra files"
+Remove-Item .\support\ -recurse
---
+++ test/Android/ZumoE2ETestApp/libs/getLibs.sh
@@ -0,0 +1,47 @@
+#! /bin/sh
+
+echo "Downloading Google Gson 2.2.2"
+curl -O http://google-gson.googlecode.com/files/google-gson-2.2.2-release.zip
+
+echo "Decompressing..."
+unzip google-gson-2.2.2-release.zip
+
+echo "Removing zip"
+rm google-gson-2.2.2-release.zip
+
+echo "Move library to destination"
+mv ./google-gson-2.2.2/gson-2.2.2.jar ./
+
+echo "Remove extra files"
+rm -rf ./google-gson-2.2.2
+
+echo "Downloading Apache HttpClient 4.2.3"
+curl -O http://apache.xfree.com.ar//httpcomponents/httpclient/binary/httpcomponents-client-4.2.3-bin.zip
+
+echo "Decompressing..."
+unzip httpcomponents-client-4.2.3-bin.zip
+
+echo "Removing zip"
+rm httpcomponents-client-4.2.3-bin.zip
+
+echo "Move library to destination"
+mv ./httpcomponents-client-4.2.3/lib/httpclient-4.2.3.jar ./
+
+echo "Remove extra files"
+rm -rf ./httpcomponents-client-4.2.3
+
+echo "Downloading Android Support V4 R11"
+curl -O https://dl-ssl.google.com/android/repository/support_r11.zip
+
+echo "Decompressing..."
+unzip support_r11.zip
+
+echo "Removing zip"
+rm support_r11.zip
+
+echo "Move library to destination"
+mv ./support/v4/android-support-v4.jar ./
+
+echo "Remove extra files"
+rm -rf ./support
+
