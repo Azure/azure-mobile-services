0ee7296ba11fe103df2ed51bf1aa22dabc19daa0
Carlos Figueira <carlosfigueirafilho@hotmail.com>
2013-03-13 20:41:19 -0700

    [PLib Test App] WP8 App (start) - Also updated NuGet package reference for PLib assemblies

---
+++ test/PLib/ZumoE2ETestApp/ZumoE2ETestAppWP8/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using ZumoE2ETestAppWP8.Resources;
+
+namespace ZumoE2ETestAppWP8
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void lstTestGroups_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void appBtnUploadLogs_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void appBtnRunTests_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void appBtnBack_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
