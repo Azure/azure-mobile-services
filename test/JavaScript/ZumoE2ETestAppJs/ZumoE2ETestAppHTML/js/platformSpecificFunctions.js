6ffefe9f256d04878123b2bee3c2f2f25f2896e4
nchejara <pchejara@live.com>
2013-05-23 02:23:08 -0700

    Add file which are untracked by git hub

---
+++ test/JavaScript/ZumoE2ETestAppJs/ZumoE2ETestAppHTML/js/platformSpecificFunctions.js
@@ -0,0 +1,33 @@
+﻿function createPlatformSpecificFunctions() {
+
+    var alertFunction;
+
+    alertFunction = function (text) {
+        window.alert(text);
+    }
+
+    var saveAppInfo = function (lastAppUrl, lastAppKey, lastUploadLogUrl) {
+        /// <param name="lastAppUrl" type="String">The last value used in the application URL text box</param>
+        /// <param name="lastAppKey" type="String">The last value used in the application key text box</param>
+        /// <param name="lastUploadLogUrl" type="String">The last value used in the upload logs URL text box</param>
+        var state = {
+            lastAppUrl: lastAppUrl,
+            lastAppKey: lastAppKey,
+            lastUploadUrl: lastUploadLogUrl
+        };
+
+        //[To do]: write file code is pending to implement, I think it is not very imporatent to write this code for HTML application
+        // Need to confirm with Carlos
+
+
+    }
+
+    return {
+        alert: alertFunction,
+        saveAppInfo: saveAppInfo,
+        IsHTMLApplication: true,
+    };
+}
+
+
+var testPlatform = createPlatformSpecificFunctions();
