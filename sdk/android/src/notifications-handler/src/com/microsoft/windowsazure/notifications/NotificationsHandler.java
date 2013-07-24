88f79e3eec3ab0f9f5eead65924a800799264b0f
Pablo Zaidenvoren <pabloz@lagash.com>
2013-07-24 11:39:00 -0300

    Javadoc for NotificationsHandler

--- sdk/android/src/notifications-handler/src/com/microsoft/windowsazure/notifications/NotificationsHandler.java
+++ sdk/android/src/notifications-handler/src/com/microsoft/windowsazure/notifications/NotificationsHandler.java
@@ -7,12 +7,29 @@ import android.os.Looper;
 import android.widget.Toast;
 
 public class NotificationsHandler {
+	
+	/**
+	 * Method called after the device is registered for notifications
+	 * @param context Application context
+	 * @param gcmRegistrationId Google Cloud Messaging registration id
+	 */
 	public void onRegistered(Context context, String gcmRegistrationId) {
 	}
 
+	/**
+	 * Method called after the device is unregistered for notifications
+	 * @param context Application context
+	 * @param gcmRegistrationId Google Cloud Messaging registration id
+	 */
 	public void onUnregistered(Context context, String gcmRegistrationId) {	
 	}
 
+	/**
+	 * Method called after a notification is received. 
+	 * By default, it shows a toast with the value asociated to the "message" key in the bundle
+	 * @param context Application Context
+	 * @param bundle Bundle with notification data
+	 */
 	public void onReceive(final Context context, final Bundle bundle) {
 		Handler h = new Handler(Looper.getMainLooper());
 		h.post(new Runnable() {
--- sdk/android/src/notifications-handler/src/com/microsoft/windowsazure/notifications/NotificationsManager.java
+++ sdk/android/src/notifications-handler/src/com/microsoft/windowsazure/notifications/NotificationsManager.java
@@ -11,18 +11,34 @@ import android.util.Log;
 
 public class NotificationsManager {
 	
+	/**
+	 * Key for handler class name in local storage 
+	 */
 	private static final String NOTIFICATIONS_HANDLER_CLASS = "WAMS_NotificationsHandlerClass";
+	
+	/**
+	 * Key for registration id in local storage
+	 */
 	private static final String GOOGLE_CLOUD_MESSAGING_REGISTRATION_ID = "WAMS_GoogleCloudMessagingRegistrationId";
 
+	/**
+	 * NotificationsHandler instance
+	 */
 	private static NotificationsHandler mHandler;
 		
-	public static <T extends NotificationsHandler> void handleNotifications(final Context context, final String gcmAppId, final Class<T> notificationHandlerClass) {
+	/**
+	 * Handles notifications with the provided NotificationsHandler class
+	 * @param context Application Context
+	 * @param gcmAppId Google Cloud Messaging Application ID
+	 * @param notificationsHandlerClass NotificationHandler class used for handling notifications
+	 */
+	public static <T extends NotificationsHandler> void handleNotifications(final Context context, final String gcmAppId, final Class<T> notificationsHandlerClass) {
 		
 		new AsyncTask<Void, Void, Void>() {
 			@Override
 			protected Void doInBackground(Void... params) {
 				try {
-					setHandler(notificationHandlerClass, context);
+					setHandler(notificationsHandlerClass, context);
 					
 					GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(context);
 
@@ -44,6 +60,10 @@ public class NotificationsManager {
 		}.execute();
 	}
 	
+	/**
+	 * Stops handlind notifications
+	 * @param context Application Context
+	 */
 	public static void stopHandlingNotifications(final Context context) {
 		
 		new AsyncTask<Void, Void, Void>() {
@@ -71,6 +91,10 @@ public class NotificationsManager {
 		}.execute();
 	}
 
+	/**
+	 * Retrieves the NotificationsHandler from local storage
+	 * @param context Application Context
+	 */
 	static NotificationsHandler getHandler(Context context) {
 		if (mHandler == null) {
 			SharedPreferences prefereneces = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
@@ -78,8 +102,8 @@ public class NotificationsManager {
 			String className = prefereneces.getString(NOTIFICATIONS_HANDLER_CLASS, null);
 			if (className != null) {
 				try {
-					Class<?> notificationHandlerClass = Class.forName(className);
-					mHandler = (NotificationsHandler) notificationHandlerClass.newInstance();
+					Class<?> notificationsHandlerClass = Class.forName(className);
+					mHandler = (NotificationsHandler) notificationsHandlerClass.newInstance();
 				} catch (Exception e) {
 					return null;
 				}
@@ -89,6 +113,10 @@ public class NotificationsManager {
 		return mHandler;
 	}
 	
+	/**
+	 * Retrieves the RegistrationId from local storage
+	 * @param context Application Context
+	 */
 	private static String getRegistrationId(Context context) {
 		SharedPreferences prefereneces = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
 
@@ -96,14 +124,25 @@ public class NotificationsManager {
 		return registrationId;
 	}
 	
-	private static <T extends NotificationsHandler> void setHandler(Class<T> notificationHandlerClass, Context context) {
+	
+	/**
+	 * Stores the NotificationsHandler class in local storage
+	 * @param notificationsHandlerClass NotificationsHandler class
+	 * @param context Application Context
+	 */
+	private static <T extends NotificationsHandler> void setHandler(Class<T> notificationsHandlerClass, Context context) {
 		SharedPreferences prefereneces = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
 
 		Editor editor = prefereneces.edit();
-		editor.putString(NOTIFICATIONS_HANDLER_CLASS, notificationHandlerClass.getName());
+		editor.putString(NOTIFICATIONS_HANDLER_CLASS, notificationsHandlerClass.getName());
 		editor.commit();
 	}
 	
+	/**
+	 * Stores the RegistrationId in local storage
+	 * @param registrationId RegistrationId to store
+	 * @param context Application Context
+	 */
 	private static void setRegistrationId(String registrationId, Context context) {
 		SharedPreferences prefereneces = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
 
