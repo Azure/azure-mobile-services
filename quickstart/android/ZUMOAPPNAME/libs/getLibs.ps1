31bce26e210122973d5f001b1016b18671de80be
Yavor Georgiev <yavorg@microsoft.com>
2013-08-07 16:32:13 -0700

    Updating to later build

--- quickstart/android/ZUMOAPPNAME/libs/getLibs.ps1
+++ quickstart/android/ZUMOAPPNAME/libs/getLibs.ps1
@@ -40,22 +40,3 @@ Move-Item "$dir\support\v4\android-support-v4.jar" "$dir"
 
 Write-Host "Remove extra files"
 Remove-Item "$dir\support\" -recurse
-
-$filename = "google_play_services_3159130_r09.zip"
-
-Write-Host "Downloading Google Play Services R9"
-$client.DownloadFile("https://dl-ssl.google.com/android/repository/google_play_services_3159130_r09.zip", "$dir\$filename") 
-
-Write-Host "Decompressing..."
-$zip_file = $shell_app.namespace("$dir\$filename")
-$destination = $shell_app.namespace("$dir")
-$destination.Copyhere($zip_file.items())
-
-Write-Host "Removing zip"
-Remove-Item "$dir\$filename"
-
-Write-Host "Move library to destination"
-Move-Item "$dir\google-play-services\libproject\google-play-services_lib\libs\google-play-services.jar" "$dir"
-
-Write-Host "Remove extra files"
-Remove-Item "$dir\google-play-services\" -recurse
