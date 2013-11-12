b39d6c44d236e47343dfeee71fa6d34519fc6bb9
Paul Batum <paul.batum@gmail.com>
2013-11-12 08:57:33 -0800

    Started work on the E2E test app for Xamarin.iOS

---
+++ test/PLib/ZumoE2ETestApp/ZumoE2ETestAppXamarinIOS/Tests/ZumoTestCommon.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using MonoTouch.Dialog;
+
+namespace ZumoE2ETestApp.Tests
+{
+    public partial class ZumoTestCommon
+    {
+        
+    }
+}
