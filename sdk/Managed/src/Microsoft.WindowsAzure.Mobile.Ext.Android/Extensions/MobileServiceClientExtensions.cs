63c75c5bebb26fe77a0afc10f88bdd7458fa0fbb
Paul Batum <paul.batum@gmail.com>
2013-11-13 22:50:59 -0800

    Made sure that both enum and string based overloads for LoginAsync are present.

--- sdk/Managed/src/Microsoft.WindowsAzure.Mobile.Ext.Android/Extensions/MobileServiceClientExtensions.cs
+++ sdk/Managed/src/Microsoft.WindowsAzure.Mobile.Ext.Android/Extensions/MobileServiceClientExtensions.cs
@@ -7,9 +7,38 @@ namespace Microsoft.WindowsAzure.MobileServices
 {
     public static class MobileServiceClientExtensions
     {
-        public static Task<MobileServiceUser> LoginAsync (this IMobileServiceClient client, Context context, string providerName)
+        /// <summary>
+        /// Log a user into a Mobile Services application given a provider name.
+        /// </summary>
+        /// <param name="barButtonItem" type="MonoTouch.UIKit.UIBarButtonItem">
+        /// UIBarButtonItem used to display a popover from on iPad.
+        /// </param>
+        /// <param name="provider" type="MobileServiceAuthenticationProvider">
+        /// Authentication provider to use.
+        /// </param>
+        /// <returns>
+        /// Task that will complete when the user has finished authentication.
+        /// </returns>
+        public static Task<MobileServiceUser> LoginAsync(this IMobileServiceClient client, Context context, MobileServiceAuthenticationProvider provider)
         {
-            var auth = new MobileServiceUIAuthentication(context, client, providerName);
+            return LoginAsync(client, context, provider.ToString());            
+        }
+
+        /// <summary>
+        /// Log a user into a Mobile Services application given a provider name.
+        /// </summary>
+        /// <param name="barButtonItem" type="MonoTouch.UIKit.UIBarButtonItem">
+        /// UIBarButtonItem used to display a popover from on iPad.
+        /// </param>
+        /// <param name="provider" type="string">
+        /// The name of the authentication provider to use.
+        /// </param>
+        /// <returns>
+        /// Task that will complete when the user has finished authentication.
+        /// </returns>
+        public static Task<MobileServiceUser> LoginAsync(this IMobileServiceClient client, Context context, string provider)
+        {
+            var auth = new MobileServiceUIAuthentication(context, client, provider);
             return auth.LoginAsync();
         }
     }
--- sdk/Managed/src/Microsoft.WindowsAzure.Mobile.Ext.iOS/Extensions/MobileServiceClientExtensions.cs
+++ sdk/Managed/src/Microsoft.WindowsAzure.Mobile.Ext.iOS/Extensions/MobileServiceClientExtensions.cs
@@ -23,9 +23,26 @@ namespace Microsoft.WindowsAzure.MobileServices
         /// <returns>
         /// Task that will complete when the user has finished authentication.
         /// </returns>
-        public static Task<MobileServiceUser> LoginAsync (this IMobileServiceClient client, UIViewController viewController, string providerName)
+        public static Task<MobileServiceUser> LoginAsync(this IMobileServiceClient client, UIViewController viewController, MobileServiceAuthenticationProvider provider)
         {
-            return LoginAsync(client, default(RectangleF), viewController, providerName);
+            return LoginAsync(client, default(RectangleF), viewController, provider.ToString());
+        }
+
+        /// <summary>
+        /// Log a user into a Mobile Services application given a provider name.
+        /// </summary>
+        /// <param name="viewController" type="MonoTouch.UIKit.UIViewController">
+        /// UIViewController used to display modal login UI on iPhone/iPods.
+        /// </param>
+        /// <param name="provider" type="string">
+        /// The name of the authentication provider to use.
+        /// </param>
+        /// <returns>
+        /// Task that will complete when the user has finished authentication.
+        /// </returns>
+        public static Task<MobileServiceUser> LoginAsync(this IMobileServiceClient client, UIViewController viewController, string provider)
+        {
+            return LoginAsync(client, default(RectangleF), viewController, provider);
         }
 
         /// <summary>
@@ -43,9 +60,29 @@ namespace Microsoft.WindowsAzure.MobileServices
         /// <returns>
         /// Task that will complete when the user has finished authentication.
         /// </returns>
-        public static Task<MobileServiceUser> LoginAsync (this IMobileServiceClient client, RectangleF rectangle, UIView view, string providerName)
+        public static Task<MobileServiceUser> LoginAsync(this IMobileServiceClient client, RectangleF rectangle, UIView view, MobileServiceAuthenticationProvider provider)
         {
-            return LoginAsync(client, rectangle, (object)view, providerName);
+            return LoginAsync(client, rectangle, (object)view, provider.ToString());
+        }
+
+        /// <summary>
+        /// Log a user into a Mobile Services application given a provider name.
+        /// </summary>
+        /// <param name="rectangle" type="System.Drawing.RectangleF">
+        /// The area in <paramref name="view"/> to anchor to.
+        /// </param>
+        /// <param name="view" type="MonoTouch.UIKit.UIView">
+        /// UIView used to display a popover from on iPad.
+        /// </param>
+        /// <param name="provider" type="string">
+        /// The name of the authentication provider to use.
+        /// </param>
+        /// <returns>
+        /// Task that will complete when the user has finished authentication.
+        /// </returns>
+        public static Task<MobileServiceUser> LoginAsync(this IMobileServiceClient client, RectangleF rectangle, UIView view, string provider)
+        {
+            return LoginAsync(client, rectangle, (object)view, provider);
         }
 
         /// <summary>
@@ -60,14 +97,31 @@ namespace Microsoft.WindowsAzure.MobileServices
         /// <returns>
         /// Task that will complete when the user has finished authentication.
         /// </returns>
-        public static Task<MobileServiceUser> LoginAsync (this IMobileServiceClient client, UIBarButtonItem barButtonItem, string providerName)
+        public static Task<MobileServiceUser> LoginAsync (this IMobileServiceClient client, UIBarButtonItem barButtonItem, MobileServiceAuthenticationProvider provider)
         {
-            return LoginAsync(client, default(RectangleF), barButtonItem, providerName);
+            return LoginAsync(client, default(RectangleF), barButtonItem, provider.ToString());
         }
 
-        internal static Task<MobileServiceUser> LoginAsync (IMobileServiceClient client, RectangleF rect, object view, string providerName)
+        /// <summary>
+        /// Log a user into a Mobile Services application given a provider name.
+        /// </summary>
+        /// <param name="barButtonItem" type="MonoTouch.UIKit.UIBarButtonItem">
+        /// UIBarButtonItem used to display a popover from on iPad.
+        /// </param>
+        /// <param name="provider" type="string">
+        /// The name of the authentication provider to use.
+        /// </param>
+        /// <returns>
+        /// Task that will complete when the user has finished authentication.
+        /// </returns>
+        public static Task<MobileServiceUser> LoginAsync(this IMobileServiceClient client, UIBarButtonItem barButtonItem, string provider)
         {
-            var auth = new MobileServiceUIAuthentication(rect, view, client, providerName);
+            return LoginAsync(client, default(RectangleF), barButtonItem, provider);
+        }
+
+        internal static Task<MobileServiceUser> LoginAsync (IMobileServiceClient client, RectangleF rect, object view, string provider)
+        {
+            var auth = new MobileServiceUIAuthentication(rect, view, client, provider);
             return auth.LoginAsync();
         }
     }
