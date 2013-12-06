e8dc7b4906ec4b4f89c1aa6a63155ba2577c2555
Hasan Khan <haskhan@microsoft.com>
2013-12-06 14:32:21 -0800

    e2e coverage for users feature

--- test/SetupScripts/tables/authenticated.read.js
+++ test/SetupScripts/tables/authenticated.read.js
@@ -1,12 +1,24 @@
-function read(query, user, request) {
+function read(query, user, request) {    
     query.where({userId: user.userId});
     request.execute({
-        success: function(results) {
-            var identities = JSON.stringify(user.getIdentities());
+        success: addIdentityToResults.bind(null, request, user)
+    });
+}
+
+function addIdentityToResults(request, user, results) {
+    user.getIdentities({
+        success: function(identities) {
+            identities = JSON.stringify(identities);
             results.forEach(function(item) {
                 item.Identities = identities;
+                item.UsersEnabled = isUsersEnabled();
             });
             request.respond();
         }
-    });
+   });    
+}
+
+function isUsersEnabled() {
+    var enabled = /\"users\"/i.test(process.env.MS_PreviewFeatures);
+    return enabled;
 }
