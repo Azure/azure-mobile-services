15faec44685d0ef58c37dc21d8ae10b2baeef472
Todd Reifsteck <toddreif@microsoft.com>
2014-02-12 15:18:50 -0800

    untracked files on push: 3b843b5 Address all outstanding Code Review feedback

---
+++ sdk/Managed/test/Microsoft.WindowsAzure.MobileServices.WindowsPhone8.Test/Functional/Push.Test.cs
@@ -0,0 +1,19 @@
+﻿// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using Microsoft.WindowsAzure.MobileServices.TestFramework;
+
+namespace Microsoft.WindowsAzure.MobileServices.Test
+{
+    [Tag("push")]
+    public class Push : FunctionalTestBase
+    {
+        [TestMethod]
+        public void TestTest()
+        {            
+        }
+    }
+}
