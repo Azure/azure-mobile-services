7a0cfe46add1367848c2588f2a4637b74392fc5a
Leonardo M. Tilli <leotilli@gmail.com>
2014-05-09 15:39:51 -0300

    Required Guava lib

--- sdk/android/src/sdk/libs/getLibs.sh
+++ sdk/android/src/sdk/libs/getLibs.sh
@@ -14,3 +14,6 @@ mv ./google-gson-2.2.2/gson-2.2.2.jar ./
 
 echo "Remove extra files"
 rm -rf ./google-gson-2.2.2
+
+echo "Downloading Guava Libraries 16.0.1"
+curl -O http://search.maven.org/remotecontent?filepath=com/google/guava/guava/16.0.1/guava-16.0.1.jar
