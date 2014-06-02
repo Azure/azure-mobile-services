47cd6ba6fa604695e75bebf41dda9c9500630bed
Leonardo M. Tilli <leotilli@gmail.com>
2014-06-02 11:34:44 -0300

    Guava dependencies

--- test/Android/ZumoE2ETestApp/libs/getLibs.ps1
+++ test/Android/ZumoE2ETestApp/libs/getLibs.ps1
@@ -41,6 +41,6 @@ Move-Item "$dir\gcm_r03\gcm-client\dist\gcm.jar" "$dir"
 Write-Host "Remove extra files"
 Remove-Item "$dir\gcm_r03\" -recurse
 
-$filename = "guava-16.0.1.jar"
-Write-Host "Downloading Guava Libraries 16.0.1"
+$filename = "guava-17.0.jar"
+Write-Host "Downloading Guava Libraries 17.0"
 $client.DownloadFile("http://search.maven.org/remotecontent?filepath=com/google/guava/guava/17.0/guava-17.0.jar", "$dir\$filename")
