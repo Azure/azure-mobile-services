c0fdd6bb7488ab9ea1ba2ae326e7e42072d6dc33
Todd Reifsteck <toddreif@microsoft.com>
2014-06-13 10:37:39 -0700

    Update iOS SDK Minor Version

--- sdk/iOS/src/WindowsAzureMobileServices.h
+++ sdk/iOS/src/WindowsAzureMobileServices.h
@@ -15,7 +15,7 @@
 #import "MSPush.h"
 
 #define WindowsAzureMobileServicesSdkMajorVersion 1
-#define WindowsAzureMobileServicesSdkMinorVersion 1
+#define WindowsAzureMobileServicesSdkMinorVersion 2
 #define WindowsAzureMobileServicesSdkBuildVersion 0
 
 #endif
