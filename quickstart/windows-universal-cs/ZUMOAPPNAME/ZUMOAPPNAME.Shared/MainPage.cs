4787f21fe022e5a9da1c43c77690b77f76d0b17a
Alexey Rodionov <alrod@microsoft.com>
2014-07-23 00:32:41 -0700

    Add C# and WinJS universal quickstarts

---
+++ quickstart/windows-universal-cs/ZUMOAPPNAME/ZUMOAPPNAME.Shared/MainPage.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.WindowsAzure.MobileServices;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace ZUMOAPPNAME
+{
+    sealed partial class MainPage: Page
+    {
+        private MobileServiceCollection<TodoItem, TodoItem> items;
+        private IMobileServiceTable<TodoItem> todoTable = App.MobileService.GetTable<TodoItem>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void InsertTodoItem(TodoItem todoItem)
+        {
+            // This code inserts a new TodoItem into the database. When the operation completes
+            // and Mobile Services has assigned an Id, the item is added to the CollectionView
+            await todoTable.InsertAsync(todoItem);
+            items.Add(todoItem);
+        }
+
+        private async void RefreshTodoItems()
+        {
+            MobileServiceInvalidOperationException exception = null;
+            try
+            {
+                // This code refreshes the entries in the list view by querying the TodoItems table.
+                // The query excludes completed TodoItems
+                items = await todoTable
+                    .Where(todoItem => todoItem.Complete == false)
+                    .ToCollectionAsync();
+            }
+            catch (MobileServiceInvalidOperationException e)
+            {
+                exception = e;
+            }
+
+            if (exception != null)
+            {
+                await new MessageDialog(exception.Message, "Error loading items").ShowAsync();
+            }
+            else
+            {
+                ListItems.ItemsSource = items;
+                this.ButtonSave.IsEnabled = true;
+            }
+        }
+
+        private async void UpdateCheckedTodoItem(TodoItem item)
+        {
+            // This code takes a freshly completed TodoItem and updates the database. When the MobileService 
+            // responds, the item is removed from the list 
+            await todoTable.UpdateAsync(item);
+            items.Remove(item);
+        }
+
+        private void ButtonRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshTodoItems();
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            var todoItem = new TodoItem { Text = TextInput.Text };
+            InsertTodoItem(todoItem);
+        }
+
+        private void CheckBoxComplete_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = (CheckBox)sender;
+            TodoItem item = cb.DataContext as TodoItem;
+            UpdateCheckedTodoItem(item);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            RefreshTodoItems();
+        }
+    }
+}
