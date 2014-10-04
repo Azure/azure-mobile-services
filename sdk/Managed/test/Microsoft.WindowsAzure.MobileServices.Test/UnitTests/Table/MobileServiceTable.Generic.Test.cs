c7d3a802352089962020e5ada1e24717aa048b09
Hasan Khan <haskhan@microsoft.com>
2014-10-03 17:37:54 -0700

    [client][managed] Support relative uri in table.read

--- sdk/Managed/test/Microsoft.WindowsAzure.MobileServices.Test/UnitTests/Table/MobileServiceTable.Generic.Test.cs
+++ sdk/Managed/test/Microsoft.WindowsAzure.MobileServices.Test/UnitTests/Table/MobileServiceTable.Generic.Test.cs
@@ -29,10 +29,21 @@ namespace Microsoft.WindowsAzure.MobileServices.Test
 
             IMobileServiceTable<ToDo> table = service.GetTable<ToDo>();
 
-            await table.ReadAsync<ToDo>("http://wwww.contoso.com/about?$filter=a eq b&$orderby=c");
+            await table.ReadAsync<ToDo>("http://www.test.com/about?$filter=a eq b&$orderby=c");
 
             Assert.AreEqual("TT,LH", hijack.Request.Headers.GetValues("X-ZUMO-FEATURES").First());
-            Assert.AreEqual("http://wwww.contoso.com/about?$filter=a eq b&$orderby=c", hijack.Request.RequestUri.ToString());
+            Assert.AreEqual("http://www.test.com/about?$filter=a eq b&$orderby=c", hijack.Request.RequestUri.ToString());
+        }
+
+        [AsyncTestMethod]
+        public async Task ReadAsync_Throws_IfAbsoluteUriHostNameDoesNotMatch()
+        {
+            IMobileServiceClient service = new MobileServiceClient("http://www.test.com", "secret...", new TestHttpHandler());
+            IMobileServiceTable<ToDo> table = service.GetTable<ToDo>();
+
+            var ex = await AssertEx.Throws<ArgumentException>(async () => await table.ReadAsync<ToDo>("http://www.contoso.com/about?$filter=a eq b&$orderby=c"));
+
+            Assert.AreEqual(ex.Message, "The query uri must be on the same host as the Mobile Service.");
         }
 
         [AsyncTestMethod]
