0e49c5cf55f6ec2388db132060759d0de5701d2b
Mariano Sánchez <sanchezmariano@gmail.com>
2014-10-20 17:29:09 -0300

    Migration to Android Studio

---
+++ sdk/android/src/sdk/build/generated/source/buildConfig/release/com/microsoft/windowsazure/mobileservices/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.microsoft.windowsazure.mobileservices;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String PACKAGE_NAME = "com.microsoft.windowsazure.mobileservices";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
