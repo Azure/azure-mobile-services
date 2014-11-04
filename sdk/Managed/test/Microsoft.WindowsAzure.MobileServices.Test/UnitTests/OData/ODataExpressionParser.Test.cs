7d831cdcbee550d9dbb808abc21d7cec7129aaff
Hasan Khan <haskhan@microsoft.com>
2014-11-03 17:21:31 -0800

    [client][managed][offline] Allow query on memer name datetime

--- sdk/Managed/test/Microsoft.WindowsAzure.MobileServices.Test/UnitTests/OData/ODataExpressionParser.Test.cs
+++ sdk/Managed/test/Microsoft.WindowsAzure.MobileServices.Test/UnitTests/OData/ODataExpressionParser.Test.cs
@@ -17,18 +17,67 @@ namespace Microsoft.WindowsAzure.MobileServices.Test.UnitTests.OData
 
             Assert.IsNotNull(queryNode);
 
-            BinaryOperatorNode comparisonNode = queryNode as BinaryOperatorNode;
+            var comparisonNode = queryNode as BinaryOperatorNode;
             Assert.IsNotNull(comparisonNode);
 
-            MemberAccessNode left = comparisonNode.LeftOperand as MemberAccessNode;
+            var left = comparisonNode.LeftOperand as MemberAccessNode;
             Assert.IsNotNull(left);
 
-            ConstantNode right = comparisonNode.RightOperand as ConstantNode;
+            var right = comparisonNode.RightOperand as ConstantNode;
             Assert.IsNotNull(right);
 
             Assert.AreEqual("Field", left.MemberName);
             Assert.AreEqual(BinaryOperatorKind.Equal, comparisonNode.OperatorKind);
             Assert.AreEqual(filterGuid, right.Value);
+        }
+
+        [TestMethod]
+        public void ParseFilter_TrueToken()
+        {
+            QueryNode queryNode = ODataExpressionParser.ParseFilter("(true eq null) and false");
+
+            Assert.IsNotNull(queryNode);
+
+            var comparisonNode = queryNode as BinaryOperatorNode;
+            Assert.IsNotNull(comparisonNode);
+
+            var left = comparisonNode.LeftOperand as BinaryOperatorNode;
+            Assert.IsNotNull(left);
+
+            var trueNode = left.LeftOperand as ConstantNode;
+            Assert.IsNotNull(trueNode);
+            Assert.AreEqual(true, trueNode.Value);
+
+            var nullNode = left.RightOperand as ConstantNode;
+            Assert.IsNotNull(nullNode);
+            Assert.AreEqual(null, nullNode.Value);
+
+            var falseNode = comparisonNode.RightOperand as ConstantNode;
+            Assert.IsNotNull(falseNode);
+
+            Assert.AreEqual(BinaryOperatorKind.And, comparisonNode.OperatorKind);
+            Assert.AreEqual(false, falseNode.Value);
+        }
+
+        [TestMethod]
+        public void ParseFilter_DateTimeMember()
+        {
+            QueryNode queryNode = ODataExpressionParser.ParseFilter("datetime eq 1");
+
+            Assert.IsNotNull(queryNode);
+
+            var comparisonNode = queryNode as BinaryOperatorNode;
+            Assert.IsNotNull(comparisonNode);
+
+            var left = comparisonNode.LeftOperand as MemberAccessNode;
+            Assert.IsNotNull(left);
+
+            var right = comparisonNode.RightOperand as ConstantNode;
+            Assert.IsNotNull(right);
+
+            Assert.AreEqual("datetime", left.MemberName);
+            Assert.AreEqual(BinaryOperatorKind.Equal, comparisonNode.OperatorKind);
+            Assert.AreEqual(1L, right.Value);
         }
 
         [TestMethod]
