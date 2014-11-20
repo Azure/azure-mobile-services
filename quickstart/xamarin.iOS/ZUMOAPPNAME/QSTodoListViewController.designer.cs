44e4899576c321244c16b3a39a6164b6b71f845d
Donna Malayeri <DonnaM@jimwan-carbon.northamerica.corp.microsoft.com>
2014-11-20 15:34:11 -0800

    Update Xamarin.iOS quickstart: add offline support, minor UI changes

--- quickstart/xamarin.iOS/ZUMOAPPNAME/QSTodoListViewController.designer.cs
+++ quickstart/xamarin.iOS/ZUMOAPPNAME/QSTodoListViewController.designer.cs
@@ -1,10 +1,13 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
+using System;
 using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.CodeDom.Compiler;
 
 namespace ZUMOAPPNAME
 {
@@ -12,25 +15,14 @@ namespace ZUMOAPPNAME
 	partial class QSTodoListViewController
 	{
 		[Outlet]
-		MonoTouch.UIKit.UIActivityIndicatorView activityIndicator { get; set; }
-
-		[Outlet]
 		MonoTouch.UIKit.UITextField itemText { get; set; }
 
 		[Action ("OnAdd:")]
-		partial void OnAdd (MonoTouch.Foundation.NSObject sender);
-		
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void OnAdd (UIButton sender);
+
 		void ReleaseDesignerOutlets ()
 		{
-			if (itemText != null) {
-				itemText.Dispose ();
-				itemText = null;
-			}
-
-			if (activityIndicator != null) {
-				activityIndicator.Dispose ();
-				activityIndicator = null;
-			}
 		}
 	}
 }
