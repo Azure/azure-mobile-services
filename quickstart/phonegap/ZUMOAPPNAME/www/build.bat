de1f950e793c01d5be41ffb50830ed85a17af91a
Phillip Van Nortwick <phillip.vannortwick@microsoft.com>
2014-11-24 09:05:31 -0800

    [Cordova QS] Use plugin

--- quickstart/phonegap/ZUMOAPPNAME/www/build.bat
+++ quickstart/phonegap/ZUMOAPPNAME/www/build.bat
@@ -1,3 +1,13 @@
-call phonegap local plugin add org.apache.cordova.inappbrowser
+REM Phonegap QS Build Script
+REM Installs the required plugins and builds the platforms supported when
+REM using Windows.
+
+call phonegap local plugin add com.microsoft.azure-mobile-services
+
+REM Plugins required for push notifications
+REM call phonegap local plugin add org.apache.cordova.device
+REM call phonegap local plugin add https://github.com/phonegap-build/PushPlugin.git
+
+REM Build supported platforms
 call phonegap local build wp8
 call phonegap local build android
