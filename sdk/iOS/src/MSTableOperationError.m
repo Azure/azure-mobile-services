84ab5a2c815fb09ebf3bcef10b5e9b93457bade9
Phillip Van Nortwick <phvannor@microsoft.com>
2015-03-30 11:22:56 -0700

    iOS: Fix cancelAndUpdate/Discard Functions
    Previously these actions on the MSTableOperationError object would
    essentially noop, now they will perform as documented

--- sdk/iOS/src/MSTableOperationError.m
+++ sdk/iOS/src/MSTableOperationError.m
@@ -14,6 +14,7 @@
 
 // Error
 @property (nonatomic) NSInteger code;
+
 @property (nonatomic) NSString *description;
 
 @property (nonatomic, copy) NSString *table;
@@ -27,80 +28,83 @@
 @property (nonatomic) NSString *rawResponse;
 @property (nonatomic) NSDictionary *serverItem;
 
-@property (nonatomic, weak) MSSyncContext *syncContext;
+@property (nonatomic, strong) MSSyncContext *syncContext;
 
 @end
 
+
 @implementation MSTableOperationError
 
-@synthesize handled = handled_;
-
-@synthesize guid = guid_;
-@synthesize code = code_;
-@synthesize description = description_;
-@synthesize table = table_;
-@synthesize operation = operation_;
-@synthesize operationId = operationId_;
-@synthesize itemId = itemId_;
-@synthesize item = item_;
-@synthesize serverItem = serverItem_;
-@synthesize statusCode = statusCode_;
-@synthesize syncContext = syncContext_;
+@synthesize description = _description;
 
 #pragma mark - Initialization
+
 
 - (id) init {
     self = [super init];
     if (self) {
-        guid_ = [MSJSONSerializer generateGUID];
+        _guid = [MSJSONSerializer generateGUID];
     }
     return self;
 }
 
-- (id) initWithOperation:(MSTableOperation *)operation item:(NSDictionary *)item error:(NSError *) error;
+- (id) initWithOperation:(MSTableOperation *)operation item:(NSDictionary *)item context:(MSSyncContext *)context error:(NSError *) error
 {
     self = [self init];
     
-    code_ = error.code;
-    description_ = [error.localizedDescription copy];
-
+    _code = error.code;
+    _description = [error.localizedDescription copy];
+    
     NSHTTPURLResponse *response = [error.userInfo objectForKey:MSErrorResponseKey];
     if (response) {
-        statusCode_ = response.statusCode;
+        _statusCode = response.statusCode;
     }
     
-    serverItem_ = [[[error userInfo] objectForKey:MSErrorServerItemKey] copy];
-
-    table_ = [operation.tableName copy];
-    operation_ = operation.type;
-    itemId_ = [operation.itemId copy];
-    item_ = [item copy];
-    operationId_ = operation.operationId;
+    _serverItem = [[[error userInfo] objectForKey:MSErrorServerItemKey] copy];
+    
+    _table = [operation.tableName copy];
+    _operation = operation.type;
+    _itemId = [operation.itemId copy];
+    _item = [item copy];
+    _operationId = operation.operationId;
+    _syncContext = context;
     
     return self;
 }
 
-- (id) initWithSerializedItem:(NSDictionary *)item
+- (id) initWithOperation:(MSTableOperation *)operation item:(NSDictionary *)item error:(NSError *) error __deprecated;
 {
+    return [[MSTableOperationError alloc] initWithOperation:operation item:item context:nil error:error];
+}
+
+- (id) initWithSerializedItem:(NSDictionary *)item context:(MSSyncContext *)context {
     self = [self init];
     if (self) {
-        guid_ = [item objectForKey:@"id"];
+        _guid = [item objectForKey:@"id"];
         
         // Unserialize the raw data now
         NSData *properties = [item objectForKey:@"properties"];
         MSJSONSerializer *serializer = [MSJSONSerializer new];
         NSDictionary *data = [serializer itemFromData:properties withOriginalItem:nil ensureDictionary:NO orError:nil];
         
-        code_ = [[data objectForKey:@"code"] integerValue];
-        description_ = [data objectForKey:@"description"];
-        table_ = [data objectForKey:@"table"];
-        operation_ = [[data objectForKey:@"operation"] integerValue];
-        itemId_ = [data objectForKey:@"itemId"];
-        item_ = [data objectForKey:@"item"];
-        serverItem_ = [data objectForKey:@"serverItem"];
-        statusCode_ = [[data objectForKey:@"statusCode"] integerValue];
+        _code = [[data objectForKey:@"code"] integerValue];
+        _description = [data objectForKey:@"description"];
+        _table = [data objectForKey:@"table"];
+        _operation = [[data objectForKey:@"operation"] integerValue];
+        _itemId = [data objectForKey:@"itemId"];
+        _item = [data objectForKey:@"item"];
+        _serverItem = [data objectForKey:@"serverItem"];
+        _statusCode = [[data objectForKey:@"statusCode"] integerValue];
+        
+        _syncContext = context;
     }
+    
     return self;
+}
+
+- (id) initWithSerializedItem:(NSDictionary *)item 
+{
+    return [[MSTableOperationError alloc] initWithSerializedItem:item context:nil];
 }
 
 - (NSDictionary *) serialize
@@ -112,7 +116,7 @@
             @"operation": [NSNumber numberWithInteger:self.operation],
             @"itemId": self.itemId,
             @"statusCode": [NSNumber numberWithInteger:self.statusCode]
-    } mutableCopy];
+        } mutableCopy];
     
     if (self.item) {
         [properties setValue:self.item forKey:@"item"];
@@ -123,17 +127,29 @@
     
     MSJSONSerializer *serializer = [MSJSONSerializer new];
     NSError *serializeError;
-    NSData *data = [serializer dataFromItem:properties idAllowed:YES ensureDictionary:NO removeSystemProperties:NO orError:&serializeError];
+    NSData *data = [serializer dataFromItem:properties
+                                  idAllowed:YES
+                           ensureDictionary:NO
+                     removeSystemProperties:NO
+                                    orError:&serializeError];
     
-    // Handle if something is wrong with one of our fields, try again without the possibly breaking fields
+    // Handle if something is wrong with one of our fields, try again without the possibly
+    // breaking fields (item or serverItem could not be serializable)
     if (serializeError.code == MSInvalidItemWithRequest) {
         [properties removeObjectForKey:@"item"];
         [properties removeObjectForKey:@"serverItem"];
         
-        data = [serializer dataFromItem:properties idAllowed:YES ensureDictionary:NO removeSystemProperties:NO orError:&serializeError];
+        data = [serializer dataFromItem:properties
+                              idAllowed:YES
+                       ensureDictionary:NO
+                 removeSystemProperties:NO
+                                orError:&serializeError];
     }
         
-    return @{ @"id": self.guid, @"operationId": [NSNumber numberWithInteger:self.operationId], @"tableKind": @0, @"properties": data };
+    return @{ @"id": self.guid,
+              @"operationId": [NSNumber numberWithInteger:self.operationId],
+              @"tableKind": @0, @"properties": data
+            };
 }
 
 #pragma mark - Error Resolution
@@ -142,13 +158,18 @@
 {
     if (!item) {
         if (completion) {
-            // TODO: Generate error
-            NSError *error = nil;
+            NSDictionary *userInfo = @{ NSLocalizedDescriptionKey: @"Item is required" };
+            NSError *error = [NSError errorWithDomain:MSErrorDomain
+                                                 code:MSSyncTableCancelError
+                                             userInfo:userInfo];
             completion(error);
+            return;
         }
     }
     
-    MSTableOperation *op = [[MSTableOperation alloc] initWithTable:self.table type:self.operation itemId:self.itemId];
+    MSTableOperation *op = [[MSTableOperation alloc] initWithTable:self.table
+                                                              type:self.operation
+                                                            itemId:self.itemId];
     op.operationId = self.operationId;
     op.item = item;
     
@@ -157,12 +178,12 @@
 
 - (void) cancelOperationAndDiscardItemWithCompletion:(MSSyncBlock)completion
 {
-    MSTableOperation *op = [[MSTableOperation alloc] initWithTable:self.table type:self.operation itemId:self.itemId];
+    MSTableOperation *op = [[MSTableOperation alloc] initWithTable:self.table
+                                                              type:self.operation
+                                                            itemId:self.itemId];
     op.operationId = self.operationId;
     
     [self.syncContext cancelOperation:op discardItemWithCompletion:completion];
 }
 
 @end
-
-
