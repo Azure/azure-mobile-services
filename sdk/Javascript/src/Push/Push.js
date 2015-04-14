65a1b2cc5d2273ff4e37032bf91ecd469d591e06
Dale Anderson <dalander@microsoft.com>
2015-04-14 11:07:53 -0700

    Code review changes

--- sdk/Javascript/src/Push/Push.js
+++ sdk/Javascript/src/Push/Push.js
@@ -12,8 +12,26 @@ function Push(client, installationId) {
     this.installationId = installationId;
 }
 
+/// <summary>
+/// Register a push channel with the Mobile Apps backend to start receiving notifications.
+/// </summary>
+/// <param name="platform" type="string">
+/// The device platform being used - wns, gcm or apns.
+/// </param>
+/// <param name="channelId" type="string">
+/// The push channel identifier or URI.
+/// </param>
+/// <param name="templates" type="string">
+/// An object containing template definitions. Template objects should contain body, headers and tags properties.
+/// </param>
+/// <param name="secondaryTiles" type="string">
+/// An object containing template definitions to be used with secondary tiles when using WNS.
+/// </param>
 Push.prototype.register = Platform.async(
-    function (platform, channelUri, templates, secondaryTiles, callback) {
+    function (platform, channelId, templates, secondaryTiles, callback) {
+        Validate.isString(platform, 'platform');
+        Validate.notNullOrEmpty(platform, 'platform');
+
         // in order to support the older callback style completion, we need to check optional parameters
         if (_.isNull(callback) && (typeof templates === 'function')) {
             callback = templates;
@@ -27,25 +45,31 @@ Push.prototype.register = Platform.async(
 
         var requestContent = {
             installationId: this.installationId,
-            pushChannel: channelUri,
+            pushChannel: channelId,
             platform: platform,
             templates: templates,
             secondaryTiles: secondaryTiles
         };
 
-        executeRequest(this.client, 'PUT', channelUri, requestContent, this.installationId, callback);
+        executeRequest(this.client, 'PUT', channelId, requestContent, this.installationId, callback);
     }
 );
 
+/// <summary>
+/// Unregister a push channel with the Mobile Apps backend to stop receiving notifications.
+/// </summary>
+/// <param name="channelId" type="string">
+/// The push channel identifier or URI.
+/// </param>
 Push.prototype.unregister = Platform.async(
-    function (channelUri, callback) {
-        executeRequest(this.client, 'DELETE', channelUri, null, this.installationId, callback);
+    function (channelId, callback) {
+        executeRequest(this.client, 'DELETE', channelId, null, this.installationId, callback);
     }
 );
 
-function executeRequest(client, method, channelUri, content, installationId, callback) {
-    Validate.isString(channelUri, 'channelUri');
-    Validate.notNullOrEmpty(channelUri, 'channelUri');
+function executeRequest(client, method, channelId, content, installationId, callback) {
+    Validate.isString(channelId, 'channelId');
+    Validate.notNullOrEmpty(channelId, 'channelId');
 
     client._request(
         method,
