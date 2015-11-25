5af5048d35e951a9344eeae8d417b2f8872bd519
Naren Soni <soninaren@live.com>
2015-11-25 08:42:05 -0800

    updating the android SDK version

--- sdk/android/src/sdk/src/main/java/com/microsoft/windowsazure/mobileservices/http/MobileServiceConnection.java
+++ sdk/android/src/sdk/src/main/java/com/microsoft/windowsazure/mobileservices/http/MobileServiceConnection.java
@@ -72,7 +72,7 @@ public class MobileServiceConnection {
     /**
      * Current SDK version
      */
-    private static final String SDK_VERSION = "2.0.2";
+    private static final String SDK_VERSION = "3.0.0";
     /**
      * The MobileServiceClient used for communication with the Mobile Service
      */
